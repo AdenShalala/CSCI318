e80ad758ffea17c2b7fb4ee2a8b55a3e537bff90
AdenShalala <as6643@uowmail.edu.au>
2025-09-05 11:47:22 +1000

    Added Initial Springboot Project to WareHost
    ToDo: Create seperate folder for HTML/Front end content
    Populate with Object Oriented layers/repositories
    Select and Implement model (Use shared API key or .gitignore local API key??)

---
+++ warehost/src/main/java/com/example/warehost/WarehostApplication.java
@@ -0,0 +1,13 @@
+package com.example.warehost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehostApplication.class, args);
+	}
+
+}
---
+++ warehost/src/test/java/com/example/warehost/WarehostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warehost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
