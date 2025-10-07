8c33ff35e627b1efe313530dd9ec4ca5c9cc8181
AdenShalala <167727799+AdenShalala@users.noreply.github.com>
2025-10-08 10:41:52 +1100

    Started sales queries

--- sales/src/main/java/com/example/sales/interfaces/rest/controller/SaleController.java
+++ sales/src/main/java/com/example/sales/interfaces/rest/controller/SaleController.java
@@ -3,8 +3,19 @@ package com.example.sales.interfaces.rest.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.sales.application.commandservices.SalesCommandService;
+import com.example.sales.application.queryservices.SalesQueryService;
+
 @Controller
 @RequestMapping("/sales")
 public class SaleController {
     //Fill with cls requests and gets
+    private SalesCommandService salesCommandService;
+    private SalesQueryService salesQueryService;
+
+    public SaleController(SalesCommandService salesCommandService, SalesQueryService salesQueryService) {
+        this.salesCommandService = salesCommandService;
+        this.salesQueryService = salesQueryService;
+    }
+    
 }
