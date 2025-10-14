65eba9eca2867a0b44ad8a837da779164b4d0690
AdenShalala <167727799+AdenShalala@users.noreply.github.com>
2025-10-14 11:25:10 +1100

    Added sharddomain dependency to sales and framework four inter domain queries

---
+++ sales/src/main/java/com/example/sales/application/outboundservices/acl/SaleItem.java
@@ -0,0 +1,14 @@
+package com.example.sales.application.outboundservices.acl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.sales.domain.model.aggregates.Sale;
+import com.example.shareddomain.Item;
+//Return to this when items has a find item by item id.
+// @Service
+// public class SaleItem {
+//     public String getItemDetails(Sale sale) {
+//         String saleID = sale.getSaleID().getSaleID();
+//         String url = "http://localhost:8081/"
+//     }
+// }
