dc5349b3968a6d2a13d522f22023f4074d892667
Henry Hocking <168395666+bruhcouch@users.noreply.github.com>
2025-10-19 15:05:19 +1100

    Created recommendation agent project

---
+++ recommendationAgent/src/main/java/com/example/recommendation_agent/RecommendationAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.recommendation_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationAgentApplication.class, args);
+	}
+
+}
