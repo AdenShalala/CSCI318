21ddb3d7702f18b037151e0e5f2fdbee228e67fe
AdenShalala <167727799+AdenShalala@users.noreply.github.com>
2025-10-20 11:51:06 +1100

    Adding Kafka support, slowly yet surely.

---
+++ sharedDomain/src/main/java/com/example/shareddomain/events/SaleCreatedEvent.java
@@ -0,0 +1,32 @@
+package com.example.shareddomain.events;
+
+import java.time.Instant;
+
+public class SaleCreatedEvent {
+
+    private String saleID;
+    private String itemID;
+    private double totalAmount;
+    private Instant timestamp;
+
+    public SaleCreatedEvent(String saleID, String itemID) {
+        this.saleID = saleID;
+        this.itemID = itemID;
+        this.timestamp = Instant.now();
+    }
+
+    public String getSaleID() { return saleID; }
+    public String getItemID() { return itemID; }
+    public double getTotalAmount() { return totalAmount; }
+    public Instant getTimestamp() { return timestamp; }
+
+    @Override
+    public String toString() {
+        return "SaleCreatedEvent{" +
+                "saleID='" + saleID + '\'' +
+                ", itemID='" + itemID + '\'' +
+                ", totalAmount=" + totalAmount +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
